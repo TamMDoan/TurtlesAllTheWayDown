e3110184b8251e426eeb829643ce789f777e64c0
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-11-29 22:53:03 -0500

    finished, ask about the times?

--- src/main/java/Turtles.java
+++ src/main/java/Turtles.java
@@ -1,7 +1,31 @@
-package PACKAGE_NAME;
+import java.time.Duration;
+import java.time.Instant;
 
 /**
  * Created by kristofer on 7/14/20.
  */
 public class Turtles {
+    public static void main(String[] args){
+        GCD gcd1 = new GCD();
+        GCD gcd2 = new GCD();
+
+        Instant start = Instant.now();
+        gcd1.recursion(21023, 306);
+        Instant stop = Instant.now();
+
+        // using nanos because .toMillis kept giving back 0
+        long timeElapsed = Duration.between(start, stop).toNanos();
+
+        Instant start2 = Instant.now();
+        gcd2.recursion(210267, 306);
+        Instant stop2 = Instant.now();
+
+        long timeElapsed2 = Duration.between(start2, stop2).toNanos();
+
+        System.out.printf("GCD #1 Time: %d nanoseconds\nGCD #2 Time: %d nanoseconds", timeElapsed, timeElapsed2);
+
+        // time is trippy?
+        // why is the second one faster than the first one?
+
+    }
 }
--- src/test/java/TurtlesTest.java
+++ src/test/java/TurtlesTest.java
@@ -1,11 +1,13 @@
+import java.time.Duration;
+import java.time.Instant;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by kristofer on 7/14/20.
  */
 public class TurtlesTest {
-    GCD gcd1 = new GCD();
-    GCD gcd2 = new GCD();
+
 
 
 
